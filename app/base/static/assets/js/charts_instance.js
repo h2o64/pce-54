f94b092f2c6b45b4084c06b326bcb8b04ab3d88b
Louis Popi <theh2o64@gmail.com>
2021-04-14 14:44:11 +0200

    home: templates: Add cloud managment

---
+++ app/base/static/assets/js/charts_instance.js
@@ -0,0 +1,42 @@
+/* eslint-disable object-curly-newline */
+
+/* global Chart */
+
+/**
+ * --------------------------------------------------------------------------
+ * CoreUI Boostrap Admin Template (v3.2.0): main.js
+ * Licensed under MIT (https://coreui.io/license)
+ * --------------------------------------------------------------------------
+ */
+
+/* eslint-disable no-magic-numbers */
+// random Numbers
+var random = function random() {
+  return Math.round(Math.random() * 100);
+}; // eslint-disable-next-line no-unused-vars
+
+
+var instanceChart = new Chart(document.getElementById('instance-chart-1'), {
+  type: 'line',
+  data: {
+    labels: ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+    datasets: [{
+      label: 'Utilisation CPU',
+      backgroundColor: 'rgba(220, 220, 220, 0.2)',
+      borderColor: 'rgba(220, 220, 220, 1)',
+      pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+      pointBorderColor: '#fff',
+      data: [random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random()]
+    }, {
+      label: 'Utilisation RAM',
+      backgroundColor: 'rgba(151, 187, 205, 0.2)',
+      borderColor: 'rgba(151, 187, 205, 1)',
+      pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+      pointBorderColor: '#fff',
+      data: [random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random()]
+    }]
+  },
+  options: {
+    responsive: true
+  }
+}); // eslint-disable-next-line no-unused-vars
